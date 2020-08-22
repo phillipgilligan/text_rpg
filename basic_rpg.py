4afcfefc82855ab683b506d6845548382a8fc05a
Phillip Gilligan <philip.giligan@outlook.com>
2020-08-22 09:43:45 -0500

    Broke down into further modules, added a profile command

--- basic_rpg.py
+++ basic_rpg.py
@@ -1,73 +1,27 @@
-import sys
 import os
 import random
-import shopinventory
+import player
 import enemies
-from rich import print as rprint
-from rich.panel import Panel
+import shopinventory
 
-class Player:
-    def __init__(self, name):
-        self.name = name
-        self.maxhealth = 100
-        self.health = self.maxhealth
-        self.base_attack = 10
-        self.gold = 0
-        self.pots = 0
-        self.weap = []
-        self.curweap = []
-        
-    @property
-    def attack(self):
-        attack = self.base_attack
-        if self.curweap == "Rusty Sword":
-            attack += 5
-            
-        if self.curweap == "Great Sword":
-            attack += 15
-            
-        if self.curweap == "Lightning Sword":
-            attack += 25
-                
-        if self.curweap == "Ultima Weapon":
-            attack += 50
-            
-        return attack
-
-def main():
-    os.system('cls')
-    print ("Welcome to my game!\n")
-    print ("1. Start")
-    print ("2. Exit")
-    option = input("-> ")
-    if option == "1":
-        start()
-    elif option == "2":
-        exit()
-    else:
-        print("Invalid selection! Please try again")
-        main()
-
+#!#################################################################################################
+#!                               Main Start System                                                #
+#!#################################################################################################
 def start():
     global PlayerIG
     os.system('cls')
     print("Hello what is your name?\n")
     option = input("-->")
-    PlayerIG = Player(option)
+    PlayerIG = player.Player(option)
     start1()
 
 def start1():
     os.system('cls')
-    print(f"Hello, how are you {PlayerIG.name}?")
-    print(f"Attack: {PlayerIG.attack}")
-    print(f"Gold: {PlayerIG.gold}")
-    print(f"Weapons: {PlayerIG.curweap}")
-    print(f"Potions: {PlayerIG.pots}")
-    print(f"Health: {PlayerIG.health}/{PlayerIG.maxhealth}")
     print("1. Fight")
     print("2. Store")
     print("3. Inventory")
-    print("4. Exit")
+    print("4. Profile")
+    print("5. Exit")
     option = input("--->")
     if option == "1":
         prefight()
@@ -76,45 +30,39 @@ def start1():
     elif option == "3":
         inventory()
     elif option == "4":
+        profile()
+    elif option == "5":
         exit()
     else:
         start1()
 
-def inventory():
+#!#################################################################################################
+#!                               Profile System                                                   #
+#!#################################################################################################    
+
+def profile():
     os.system('cls')
-    print("What do you want to do?")
-    print("1. Equip Weapon")
-    print ("b. Go back")
-    option = input(">>>")
-    if option == "1":
-        equip()
-    elif option == "b":
+    print(f"Hello, how are you {PlayerIG.name}?")
+    print("-"*35)
+    print(f"Health: {PlayerIG.health}/{PlayerIG.maxhealth}")
+    print(f"Weapon: {PlayerIG.curweap}")
+    print(f"Attack: {PlayerIG.attack}")
+    print(f"Armor: {PlayerIG.curarmor}")
+    print(f"Defense: {PlayerIG.defense}")
+    print(f"Gold: {PlayerIG.gold}")
+    print(f"Potions: {PlayerIG.pots}")
+    print("-"*35)
+    print("b. Exit")
+    option = input("->")
+    if option == "b":
         start1()
     else:
-        inventory()
+        profile()
+    
 
-def equip():
-    os.system('cls')
-    print("What do you want to equip?")
-    for weapon in PlayerIG.weap:
-        print (weapon)
-    print("b to go back")
-    option = input(">>> ")
-    if option == PlayerIG.curweap:
-        print ("You alread have that weapon equipped!")
-        input("")
-        equip()
-    elif option == "b":
-        inventory()
-    elif option in PlayerIG.weap:
-        PlayerIG.curweap = option
-        print (f"You have equipped {option}!")
-        input("")
-        equip()
-    else:
-        print (f"You don't have {option} in your inventory!")
-        equip()
-
+#!#################################################################################################
+#!                               Fighting System                                                  #
+#!#################################################################################################        
 def prefight():
     global enemy
     enemynum = random.randint(1, 2)
@@ -146,8 +94,6 @@ def attack():
     os.system('cls')
     pAttack = random.randint(int(PlayerIG.attack / 2), PlayerIG.attack)
     eAttack = random.randint(int(enemy.attack / 2), enemy.attack)
-    # #pAttack = random.randint(1, 10)
-    # #eAttack = random.randint(1, 10)
     
     if pAttack == PlayerIG.attack / 2:
         print("You miss!")
@@ -172,20 +118,6 @@ def attack():
         lose()
     else:
         fight()
-
-def win():
-    enemy.health = enemy.maxhealth
-    PlayerIG.gold += enemy.goldgain
-    print(f"You have defeated the {enemy.name}!")
-    print(f"You have gained {enemy.goldgain} gold!")
-    input("")
-    start1()
-
-def lose():
-    enemy.health = enemy.maxhealth
-    print ("You have Died!")
-    input("")   
-    start()
 
 def drinkpot():
     os.system('cls')
@@ -224,6 +156,23 @@ def flee():
         else:
             fight()
 
+def win():
+    enemy.health = enemy.maxhealth
+    PlayerIG.gold += enemy.goldgain
+    print(f"You have defeated the {enemy.name}!")
+    print(f"You have gained {enemy.goldgain} gold!")
+    input("")
+    start1()
+
+def lose():
+    enemy.health = enemy.maxhealth
+    print ("You have Died!")
+    input("")   
+    start()
+
+#!#################################################################################################
+#!                               Store System                                                     #
+#!#################################################################################################           
 def store():
     os.system('cls')
     print("Welcome to the shop! \nWhat would you like?")
@@ -238,16 +187,17 @@ def store():
         buyarmor()
     elif option == "3":
         buyitems()
+    elif option == "b":
+        start1()
     else:
         store()
-    
+
 def buysword():   
     input("")
     os.system('cls')
     for item in shopinventory.sword:
         print (f"{item}, {shopinventory.sword[item]}G")
     print("b. Leave")
-    print ("")
     option = input("->")
     if option == "b":
         start1()
@@ -257,7 +207,6 @@ def buysword():
             PlayerIG.gold -= shopinventory.sword[option]
             PlayerIG.weap.append(option)
             print(f"Purchased {option}!")
-            input("")
             store()
         else:
             os.system('cls')
@@ -269,7 +218,7 @@ def buysword():
         print("This item does not exist!")
         input("")
         store()
-        
+
 def buyarmor():   
     input("")
     os.system('cls')
@@ -284,7 +233,7 @@ def buyarmor():
         if PlayerIG.gold >= shopinventory.armor[option]:
             os.system('cls')
             PlayerIG.gold -= shopinventory.armor[option]
-            PlayerIG.weap.append(option)
+            PlayerIG.armor.append(option)
             print(f"Purchased {option}!")
             input("")
             store()
@@ -298,7 +247,7 @@ def buyarmor():
         print("This item does not exist!")
         input("")
         store()
-        
+
 def buyitems():   
     input("")
     os.system('cls')
@@ -313,7 +262,7 @@ def buyitems():
         if PlayerIG.gold >= shopinventory.items[option]:
             os.system('cls')
             PlayerIG.gold -= shopinventory.items[option]
-            PlayerIG.weap.append(option)
+            PlayerIG.pots += 1
             print(f"Purchased {option}!")
             input("")
             store()
@@ -327,4 +276,61 @@ def buyitems():
         print("This item does not exist!")
         input("")
         store()
+
+#!#################################################################################################
+#!                               Inventory System                                                 #
+#!#################################################################################################    
+def inventory():
+    os.system('cls')
+    print(f"Weapons: {PlayerIG.curweap}")
+    print(f"Armor: {PlayerIG.curarmor}")
+    print("What do you want to do?")
+    print("1. Equip Weapon")
+    print ("b. Go back")
+    option = input(">>>")
+    if option == "1":
+        equip()
+    elif option == "b":
+        start1()
+    else:
+        inventory()
+
+def equip():
+    os.system('cls')
+    print("What do you want to equip?")
+    for weapon in PlayerIG.weap:
+        print (weapon)
+    print("b to go back")
+    option = input(">>> ")
+    if option == PlayerIG.curweap:
+        print ("You alread have that weapon equipped!")
+        input("")
+        equip()
+    elif option == "b":
+        inventory()
+    elif option in PlayerIG.weap:
+        PlayerIG.curweap = option
+        print (f"You have equipped {option}!")
+        input("")
+        equip()
+    else:
+        print (f"You don't have {option} in your inventory!")
+        equip()
+
+#!#################################################################################################
+#!                               Main Function                                                    #
+#!#################################################################################################    
+def main():
+    os.system('cls')
+    print ("Welcome to my game!\n")
+    print ("1. Start")
+    print ("2. Exit")
+    option = input("-> ")
+    if option == "1":
+        start()
+    elif option == "2":
+        exit()
+    else:
+        print("Invalid selection! Please try again")
+        main()
 main()
---
+++ player.py
@@ -0,0 +1,58 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.maxhealth = 100
+        self.health = self.maxhealth
+        self.base_attack = 10
+        self.base_defense = 10
+        self.gold = 0
+        self.pots = 0
+        self.weap = []
+        self.curweap = []
+        self.armor = []
+        self.curarmor = []
+
+    @property
+    def attack(self):
+        attack = self.base_attack
+        if self.curweap == "Small Dagger":
+            attack += 5
+
+        if self.curweap == "Copper Sword":
+            attack += 10
+
+        if self.curweap == "Iron Sword":
+            attack += 15
+
+        if self.curweap == "Bronze Sword":
+            attack += 20
+
+        if self.curweap == "Steel Sword":
+            attack += 25
+        
+        if self.curweap == "Diamond Sword":
+            attack += 30
+
+        return attack
+    @property
+    def defense(self):
+        defense = self.base_defense
+        if self.curweap == "Leather Armor":
+            defense += 5
+
+        if self.curweap == "Copper Armor":
+            defense += 10
+
+        if self.curweap == "Iron Armor":
+            defense += 15
+
+        if self.curweap == "Bronze Armor":
+            defense += 20
+
+        if self.curweap == "Steel Armor":
+            defense += 25
+        
+        if self.curweap == "Diamond Armor":
+            defense += 30
+
+        return defense
